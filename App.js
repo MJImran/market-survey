5c5f29a4720c19a68de1c82d11174d5f6709cf6c
MJImran <145844122+MJImran@users.noreply.github.com>
2024-09-23 11:19:07 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Components/navbar";
+import MyHeader from "./Components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <MyHeader />
+      <Navbar />
+    </div>
+  );
+}
+export default App;
