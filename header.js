08b2be48c88e76f92a73601429167b700cf1642e
MJImran <145844122+MJImran@users.noreply.github.com>
2024-09-23 11:19:48 +0100

    Add files via upload

---
+++ header.js
@@ -0,0 +1,14 @@
+const MyHeader = () => {
+  return (
+    <header className="head-section">
+      <div className="logo">
+        <p>NALOT</p>
+      </div>
+      <div className="title">
+        <h1>NALOT PRICE LIBRARY</h1>
+      </div>
+    </header>
+  );
+};
+
+export default MyHeader;
---
+++ navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-content">
+        <li>
+          <a href="#">SUPPLIER</a>
+        </li>
+        <li>
+          <a href="#">ESTIMATE MATERIALS</a>
+        </li>
+        <li>
+          <a href="#">COST LIBRARY</a>
+        </li>
+        <li>
+          <a href="#">ENQUIRIES</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
